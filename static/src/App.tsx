d6f8940004f58d38ae90b52ab31f57ba088d9cd0
YongBeomKim <saltman21@naver.com>
2021-08-28 11:53:18 +0900

    react tdd with django

---
+++ static/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+// https://www.npmjs.com/package/typed-css-modules
+import './index.css'
+
+const test = process.env.HI
+
+export default function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
